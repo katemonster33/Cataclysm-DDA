254b13f6e1f4ac6142395cb4590a682d6fe0b95f
Katie M <mckeanke@msu.edu>
2023-04-10 00:43:46 -0400

    Adding new helper class for authoring UIs.

---
+++ src/cata_ui.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "ui_manager.h"
+
+namespace cata_ui
+{
+    class dialog
+    {
+        ui_adaptor adaptor;
+        class input_context* ctx;
+        void on_redraw( ui_adaptor& adaptor );
+
+    public:
+        dialog(std::string& input_context);
+
+        ui_fragmant* add_button( point p, std::string& text, std::string& action );
+        ui_fragmant* add_button_toggle( point p, std::string& text, std::string& action, int toggle_group_index );
+        ui_fragmant* add_text_field( point p, std::string& text, int width = -1, bool selectable = false );
+        ui_fragmant* add_text_multiline( point p, std::string& text, int width, int height );
+        ui_fragmant* add_edit_field( point p, int width );
+
+        void show();
+    };
+    enum class ui_fragment_type {
+        button,
+        button_toggle,
+        text,
+        text_multiline,
+        edit
+    };
+    class ui_fragmant
+    {
+    public:
+        ui_fragmant( ui_fragment_type t, point p, std::string& text );
+        ui_fragmant( ui_fragment_type t, point p, std::string& text, std::string& action );
+
+        dialog parent;
+
+        bool show_mnemonic;
+
+        ui_fragment_type get_fragment_type();
+        point get_point();
+        std::string get_text();
+    };
+}
