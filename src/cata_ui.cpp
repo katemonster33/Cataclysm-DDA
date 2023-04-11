c3614528fdb0312b8626e28ee0ea72cd137f0c5c
Katie M <mckeanke@msu.edu>
2023-04-10 20:29:50 -0400

    new UI WIP

--- src/cata_ui.cpp
+++ src/cata_ui.cpp
@@ -1,67 +1,155 @@
 #include "cata_ui.h"
+#include "ui_manager.h"
+#include "input.h"
 
-
-void cata_ui::dialog::on_redraw(ui_adaptor &adaptor)
+void cata_ui::dialog::register_action_handler( const std::string &action_id,
+        std::function<void()> callback )
 {
-
+    action_handler_map[action_id] = { translation(), callback };
 }
 
-cata_ui::dialog::dialog(std::string &input_context)
+void cata_ui::dialog::register_action_handler( const std::string &action_id, const translation t,
+        std::function<void()> callback )
 {
-
+    action_handler_map[action_id] = { t, callback };
 }
 
-cata_ui::ui_fragmant *cata_ui::dialog::add_button(point p, std::string &text, std::string &action)
+void cata_ui::dialog::close()
 {
-
+    is_open = false;
 }
 
-cata_ui::ui_fragmant *cata_ui::dialog::add_button_toggle(point p, std::string &text, std::string &action, int toggle_group_index)
+cata_ui::dialog::dialog( std::string &category )
 {
-
+    is_open = true;
 }
 
-cata_ui::ui_fragmant *cata_ui::dialog::add_text_field(point p, std::string &text, int width, bool selectable)
+cata_ui::dialog::~dialog()
 {
-
+    is_open = false;
+    for(cata_ui::fragment *fragment : controls)
+    {
+        delete fragment;
+    }
 }
 
-cata_ui::ui_fragmant *cata_ui::dialog::add_text_multiline(point p, std::string &text, int width, int height)
+void cata_ui::dialog::on_click_default_handler( fragment *fragment )
 {
-
+    
 }
 
-cata_ui::ui_fragmant *cata_ui::dialog::add_edit_field(point p, int width)
+cata_ui::fragment *cata_ui::dialog::add_button( point p, std::string &text, std::string &action )
 {
+    return controls.emplace_back( new cata_ui::fragment(cata_ui::fragment_type::button, p, text,
+        action));
+}
 
+cata_ui::fragment *cata_ui::dialog::add_button_toggle( point p, std::string &text,
+        std::string &action, int toggle_group_index )
+{
+    return controls.emplace_back( new cata_ui::fragment( cata_ui::fragment_type::button_toggle, p, text,
+                                  action ) );
+}
+
+cata_ui::fragment *cata_ui::dialog::add_text_field( point p, std::string &text, int width,
+        bool selectable )
+{
+    cata_ui::fragment* fragment = controls.emplace_back( new cata_ui::fragment(cata_ui::fragment_type::text, p, text));
+    fragment->selectable = selectable;
+    return fragment;
+}
+
+cata_ui::fragment *cata_ui::dialog::add_text_multiline( point p, std::string &text, int width,
+        int height )
+{
+    return nullptr;
+}
+
+cata_ui::fragment *cata_ui::dialog::add_edit_field( point p, int width )
+{
+    return nullptr;
+}
+
+cata_ui::fragment *cata_ui::dialog::get_active_fragment()
+{
+    return active;
+}
+
+void cata_ui::dialog::on_redraw(ui_adaptor *adaptor)
+{
+    for(fragment *ctrl : controls)
+    {
+
+    }
 }
 
 void cata_ui::dialog::show()
 {
+    ui_adaptor adaptor;
+    input_context ctx( category );
+    for(std::pair<std::string, std::pair<translation, std::function<void()>>> kv : action_handler_map)
+    {
+        ctx.register_action(kv.first, kv.second.first);
+    }
+    adaptor.on_redraw( [this]( ui_adaptor & adaptor ) {
+        on_redraw(&adaptor);
+    } );
 
+    while( is_open ) {
+        const std::string input = ctx.handle_input();
+
+        auto handled_input = action_handler_map.find(input);
+        if(handled_input != action_handler_map.end())
+        {
+            handled_input->second.second();
+        }
+
+        adaptor.redraw_invalidated();
+    }
 }
 
-cata_ui::ui_fragmant::ui_fragmant(cata_ui::ui_fragment_type t, point p, std::string &text)
+cata_ui::fragment::fragment( cata_ui::fragment_type t, point p, std::string &text )
 {
-
+    this->type = t;
+    this->location = p;
+    this->text = text;
 }
 
-cata_ui::ui_fragmant::ui_fragmant(cata_ui::ui_fragment_type t, point p, std::string &text, std::string &action)
+cata_ui::fragment::fragment( cata_ui::fragment_type t, point p, std::string &text,
+                             std::string &action )
 {
-
+    this->type = t;
+    this->location = p;
+    this->text = text;
+    this->action = action;
 }
 
-cata_ui::ui_fragment_type cata_ui::ui_fragmant::get_fragment_type()
+cata_ui::fragment_type cata_ui::fragment::get_fragment_type()
 {
-
+    return type;
 }
 
-point cata_ui::ui_fragmant::get_location()
+point cata_ui::fragment::get_location()
 {
     return location;
 }
 
-std::string cata_ui::ui_fragmant::get_text()
+std::string cata_ui::fragment::get_text()
 {
     return text;
+}
+
+std::string cata_ui::fragment::get_action()
+{
+    return action;
+}
+
+bool cata_ui::fragment::run_custom_click_handler()
+{
+    if(on_click)
+    {
+        on_click.value();
+        return true;
+    }
+    return false;
 }
--- src/cata_ui.h
+++ src/cata_ui.h
@@ -1,46 +1,78 @@
 #pragma once
-#include "ui_manager.h"
+#include "point.h"
+#include "translation.h"
+#include <map>
+
+class ui_adaptor;
+class input_context;
 
 namespace cata_ui
 {
-    class dialog
-    {
-        ui_adaptor adaptor;
-        class input_context* ctx;
-        void on_redraw( ui_adaptor& adaptor );
+enum class fragment_type {
+    button,
+    button_toggle,
+    text,
+    text_multiline,
+    edit
+};
 
-    public:
-        dialog(std::string& input_context);
-
-        ui_fragmant* add_button( point p, std::string& text, std::string& action );
-        ui_fragmant* add_button_toggle( point p, std::string& text, std::string& action, int toggle_group_index );
-        ui_fragmant* add_text_field( point p, std::string& text, int width = -1, bool selectable = false );
-        ui_fragmant* add_text_multiline( point p, std::string& text, int width, int height );
-        ui_fragmant* add_edit_field( point p, int width );
-
-        void show();
-    };
-    enum class ui_fragment_type {
-        button,
-        button_toggle,
-        text,
-        text_multiline,
-        edit
-    };
-    class ui_fragmant
-    {
+class fragment
+{
+        fragment_type type;
         std::string text;
+        std::string action;
         point location;
-    public:
-        ui_fragmant( ui_fragment_type t, point p, std::string& text );
-        ui_fragmant( ui_fragment_type t, point p, std::string& text, std::string& action );
 
-        dialog parent;
+    public:
+        fragment( fragment_type t, point p, std::string &text );
+        fragment( fragment_type t, point p, std::string &text, std::string &action );
+        fragment( fragment_type t, point p, std::string &text, std::function<void()> custom_click_handler );
+        class dialog *parent;
 
         bool show_mnemonic;
+        bool enabled;
+        bool selectable;
+        std::optional<std::function<void()>> on_click;
+        std::optional<std::function<void()>> on_select;
 
-        ui_fragment_type get_fragment_type();
+        fragment_type get_fragment_type();
         point get_location();
         std::string get_text();
-    };
+        std::string get_action();
+        bool run_custom_click_handler();
+};
+
+class dialog
+{
+        std::map<std::string, std::pair<translation, std::function<void()>>> action_handler_map;
+        std::vector<fragment*> controls;
+        std::string category;
+        fragment *active;
+        bool is_open;
+        bool trim_width;
+
+        void on_click_default_handler( fragment *fragment );
+        void on_redraw(ui_adaptor *adaptor);
+
+    public:
+        void register_action_handler( const std::string &action_id, std::function<void()> callback );
+        void register_action_handler( const std::string &action_id, const translation t,
+                                      std::function<void()> callback );
+        dialog( std::string &category );
+        ~dialog();
+
+        fragment *add_button( point p, std::string &text, std::string &action );
+        fragment *add_button_toggle( point p, std::string &text, std::string &action,
+                                     int toggle_group_index );
+        fragment *add_text_field( point p, std::string &text, int width = -1, bool selectable = false );
+
+        fragment *add_text_multiline( point p, std::string &text, int width, int height );
+        fragment *add_edit_field( point p, int width );
+
+        fragment *get_active_fragment();
+
+        void show();
+
+        void close();
+};
 }
