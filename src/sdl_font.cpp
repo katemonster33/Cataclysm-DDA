16d51c06bd3e243bfe6c04ddabd69bfb502f2a99
Kate M <mckeanke@msu.edu>
2023-05-21 01:57:50 -0400

    Fixed issues compiling on Linux, astyle regressions

--- src/sdl_font.cpp
+++ src/sdl_font.cpp
@@ -1,4 +1,4 @@
-#if defined(TILES)
+﻿#if defined( TILES )
 #include "sdl_font.h"
 
 #include "font_loader.h"
@@ -19,9 +19,9 @@
 
 #define dbg(x) DebugLog((x),D_SDL) << __FILE__ << ":" << __LINE__ << ": "
 
-// bitmap font size test
-// return face index that has this size or below
-static int test_face_size( const std::string &f, int size, int faceIndex )
+    // bitmap font size test
+    // return face index that has this size or below
+    static int test_face_size( const std::string &f, int size, int faceIndex )
 {
     const TTF_Font_Ptr fnt( TTF_OpenFontIndex( f.c_str(), size, faceIndex ) );
     if( fnt ) {
