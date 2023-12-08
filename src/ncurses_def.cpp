362fbb955387c3468c33f20d8c47644e58614bcc
Kate M <mckeanke@msu.edu>
2023-12-07 20:50:16 -0500

    Fixed a race condition related to mouse events

--- src/ncurses_def.cpp
+++ src/ncurses_def.cpp
@@ -329,12 +329,6 @@ void catacurses::init_interface()
     if( !newscr ) {
         throw std::runtime_error( "null newscr" );
     }
-#if !defined(__CYGWIN__)
-    // ncurses mouse registration
-    mouseinterval( 0 );
-    mousemask( ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, nullptr );
-    printf( "\033[?1003h\n" );
-#endif
     // our curses wrapper does not support changing this behavior, ncurses must
     // behave exactly like the wrapper, therefore:
     noecho();  // Don't echo keypresses
@@ -352,6 +346,13 @@ void catacurses::init_interface()
     ImTui_ImplText_Init();
 
     ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+#if !defined(__CYGWIN__)
+    // ncurses mouse registration
+    mouseinterval( 0 );
+    mousemask( ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, nullptr );
+    printf( "\033[?1003h\n" );
+#endif
 }
 
 bool catacurses::supports_256_colors()
