5e9b77c80a4014194b0c9136df07b169d9decace
Katie M <mckeanke@msu.edu>
2024-02-10 00:47:49 -0500

    Modifying Gradle build for ImGui
    Co-authored-by: Brambor <tondadrd@seznam.cz>
    Co-authored-by: akrieger <akrieger@users.noreply.github.com>

--- android/app/jni/src/third-party/imgui/Android.mk
+++ android/app/jni/src/third-party/imgui/Android.mk
@@ -4,15 +4,17 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/third-party
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 
-LOCAL_MODULE := third-party
+LOCAL_MODULE := imgui
 
 LOCAL_CPP_FEATURES := exceptions rtti
 
-# Add your application source files here...
-FLATBUFFERS_SRCS := $(sort $(wildcard $(LOCAL_PATH)/third-party/flatbuffers/*.cpp))
-LOCAL_SRC_FILES := $(sort $(FLATBUFFERS_SRCS:$(LOCAL_PATH)/%=%))
+LOCAL_SHARED_LIBRARIES := SDL2
 
-LOCAL_CFLAGS += -DBACKTRACE=1 -DLOCALIZE=1 -Wextra -Wall -fsigned-char
+# Add your application source files here...
+IMGUI_SRCS := $(sort $(wildcard $(LOCAL_PATH)/third-party/imgui/*.cpp))
+LOCAL_SRC_FILES := $(sort $(IMGUI_SRCS:$(LOCAL_PATH)/%=%))
+
+LOCAL_CFLAGS += -DBACKTRACE=1 -DLOCALIZE=1 -fsigned-char
 
 ifeq ($(OS),Windows_NT)
     # needed to bypass 8191 character limit on Windows command line
