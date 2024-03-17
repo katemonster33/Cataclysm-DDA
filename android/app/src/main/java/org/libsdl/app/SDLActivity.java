af213337a00d874c3454540669b08731a6b9ce95
Kate <mckeanke@msu.edu>
2024-03-17 14:46:03 -0400

    Update SDLActivity.java hack 5

--- android/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ android/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -793,10 +793,9 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
                         if (window != null) {
                             if ((msg.obj instanceof Integer) && ((Integer) msg.obj != 0)) {
                                 int flags = View.SYSTEM_UI_FLAG_FULLSCREEN |
-                                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                                        
                                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                            
                                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.INVISIBLE;
                                 window.getDecorView().setSystemUiVisibility(flags);
                                 window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
